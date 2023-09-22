59486af938aa9de50dbd52c38715ff16a87230fa
hyujikoh <dvum0045@gmail.com>
2023-09-22 16:53:02 +0900

    path 를 통한 파일 접근

--- myapp/index.js
+++ myapp/index.js
@@ -1,6 +1,13 @@
 import express from "express";
+import path from 'path'
 
-const app = Express();
+const __dirname = path.resolve();
+const app = express();
 
 app.listen(3000);
 
+app.get('/',(req,res) => 
+{
+    res.sendFile(__dirname + '/public/main.html')
+}
+);
