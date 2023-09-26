af8ed07d51e2f827333ec3a4605e2f885b16ea64
hyujikoh <dvum0045@gmail.com>
2023-09-26 16:43:44 +0900

    write 페이지 구현

--- myapp/index.js
+++ myapp/index.js
@@ -1,14 +1,31 @@
 import express from "express";
-import path from 'path'
+import path from 'path';
+import nunjucks from 'nunjucks';
 
 const __dirname = path.resolve();
 const app = express();
 
+// view engin set
+app.set('view engine', 'html');
+
+//nujucks 
+nunjucks.configure('views' , {
+
+    watch: true, // html 파일이 수정될 경우, 다시 반영 후 랜더링
+    express: app
+});
 
 app.get('/',(req,res) => 
 {
-    res.sendFile(__dirname + '/public/main.html')
+    res.sendFile(__dirname + '/public/main.html');
 }
+);
+
+app.get('/write',(req,res) => 
+{
+    res.render('write.html');
+}
+
 );
 
 app.listen(3000, () => {
